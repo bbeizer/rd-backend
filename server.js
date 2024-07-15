85581e5e0e1d570abeb502f6db53381f143e6761
bbeizer123 <ben.beizer@sycamoreinformatics.com>
2024-07-14 20:13:09 -0400

    reverted back

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const helmet = require('helmet');
 const cors = require('cors');
 const morgan = require('morgan');
-const path = require('path');
 const gameRoutes = require('./routes/gameRoutes');
 
 const waitingPlayers = [];
@@ -22,16 +21,8 @@ mongoose.connect(process.env.DB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error(err));
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
-
 // Routes
 app.use('/api/games', gameRoutes);
-
-// The "catchall" handler: for any request that doesn't match one above, send back index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
 
 // Start server
 const PORT = process.env.PORT || 5000;
