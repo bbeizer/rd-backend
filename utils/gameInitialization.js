5dc64a1cf14d9a75fac26aff011a6d955caed18b
bbeizer123 <ben.beizer@sycamoreinformatics.com>
2024-08-16 15:16:49 -0400

    minor change to gameInitialization

--- utils/gameInitialization.js
+++ utils/gameInitialization.js
@@ -1,7 +1,7 @@
 // utils/gameInitialization.js
 
 const uuidv4 = require('uuid').v4; 
-const initialSetup = (rowIndex, colIndex, cellKey)  => {
+const initialSetup = (rowIndex, colIndex) => {
     let pieceColor = null;
     let hasBall = false;
     let id = null;  // Default to no ID unless a piece is created
@@ -16,7 +16,7 @@ const initialSetup = (rowIndex, colIndex, cellKey)  => {
         id = uuidv4();  // Assign a unique ID
     }
 
-    return { pieceColor, hasBall, cellKey };
+    return { pieceColor, hasBall };
 };
 
 const initializeBoardStatus = () => {
@@ -25,9 +25,9 @@ const initializeBoardStatus = () => {
     for (let rowIndex = 0; rowIndex < 8; rowIndex++) {
         for (let colIndex = 0; colIndex < 8; colIndex++) {
             const cellKey = `${String.fromCharCode(97 + colIndex)}${rowIndex + 1}`;
-            const { pieceColor, hasBall, position} = initialSetup(rowIndex, colIndex, cellKey);
+            const { pieceColor, hasBall, position } = initialSetup(rowIndex, colIndex);
             if (pieceColor) {
-                initialBoardStatus[cellKey] = { color: pieceColor, hasBall, position: position};
+                initialBoardStatus[cellKey] = { color: pieceColor, hasBall, position: cellKey};
             } else {
                 initialBoardStatus[cellKey] = null;
             }
