3021d932ea8db2b53fd98da717e6c714b5805cd6
bbeizer123 <ben.beizer@sycamoreinformatics.com>
2024-08-18 16:20:46 -0400

    small adjustment to match cellKey up with regular chess

--- utils/gameInitialization.js
+++ utils/gameInitialization.js
@@ -1,22 +1,21 @@
-// utils/gameInitialization.js
-
 const uuidv4 = require('uuid').v4; 
+
 const initialSetup = (rowIndex, colIndex) => {
     let pieceColor = null;
     let hasBall = false;
-    let id = null;  // Default to no ID unless a piece is created
+    let id = null;
 
-    if (rowIndex === 0 && [2, 3, 4, 5].includes(colIndex)) {
+    if (rowIndex === 1 && [2, 3, 4, 5].includes(colIndex)) {
         pieceColor = 'black';
         hasBall = colIndex === 4;
-        id = uuidv4();  // Assign a unique ID
-    } else if (rowIndex === 7 && [2, 3, 4, 5].includes(colIndex)) {
+        id = uuidv4(); 
+    } else if (rowIndex === 6 && [2, 3, 4, 5].includes(colIndex)) {
         pieceColor = 'white';
         hasBall = colIndex === 3;
-        id = uuidv4();  // Assign a unique ID
+        id = uuidv4();
     }
 
-    return { pieceColor, hasBall };
+    return { pieceColor, hasBall, id };
 };
 
 const initializeBoardStatus = () => {
@@ -24,10 +23,10 @@ const initializeBoardStatus = () => {
 
     for (let rowIndex = 0; rowIndex < 8; rowIndex++) {
         for (let colIndex = 0; colIndex < 8; colIndex++) {
-            const cellKey = `${String.fromCharCode(97 + colIndex)}${rowIndex + 1}`;
-            const { pieceColor, hasBall } = initialSetup(rowIndex, colIndex);
+            const cellKey = `${String.fromCharCode(97 + colIndex)}${8 - rowIndex}`;
+            const { pieceColor, hasBall, id } = initialSetup(rowIndex, colIndex);
             if (pieceColor) {
-                initialBoardStatus[cellKey] = { color: pieceColor, hasBall, position: cellKey};
+                initialBoardStatus[cellKey] = { color: pieceColor, hasBall, position: cellKey, id };
             } else {
                 initialBoardStatus[cellKey] = null;
             }
@@ -35,7 +34,6 @@ const initializeBoardStatus = () => {
     }
 
     return {
-        // Assuming you'd assign a unique ID upon saving to the database
         status: 'not started',
         turnPlayer: 'white',
         moveHistory: [],
