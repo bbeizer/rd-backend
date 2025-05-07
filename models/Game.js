c8fb8c9535afb6926fea92c8fbfc47d7ebfec98f
bbeizer123 <ben.beizer@sycamoreinformatics.com>
2025-05-06 21:06:40 -0400

    add conversation field to game schema

--- models/Game.js
+++ models/Game.js
@@ -8,29 +8,29 @@ const gameSchema = new mongoose.Schema({
   },
 
   gameType: { type: String, required: true, enum: ['single', 'multiplayer'], default: 'multiplayer' },
-
+  conversation: { type: Array, default: [] },
   currentBoardStatus: {
     type: Map,
     of: new mongoose.Schema({
       color: String,
       hasBall: Boolean,
       position: String
-    }, { _id: false }), // 🔹 Disables Mongoose from adding `_id` to each piece
+    }, { _id: false }),
     default: {
-      a1: null, b1: null, 
+      a1: null, b1: null,
       c1: { color: "white", hasBall: false, position: "c1" },
       d1: { color: "white", hasBall: true, position: "d1" },
       e1: { color: "white", hasBall: false, position: "e1" },
       f1: { color: "white", hasBall: false, position: "f1" },
       g1: null, h1: null,
-  
-      a8: null, b8: null, 
+
+      a8: null, b8: null,
       c8: { color: "black", hasBall: false, position: "c8" },
       d8: { color: "black", hasBall: false, position: "d8" },
       e8: { color: "black", hasBall: true, position: "e8" },
       f8: { color: "black", hasBall: false, position: "f8" },
       g8: null, h8: null,
-  
+
       a7: null, b7: null, c7: null, d7: null, e7: null, f7: null, g7: null, h7: null,
       a6: null, b6: null, c6: null, d6: null, e6: null, f6: null, g6: null, h6: null,
       a5: null, b5: null, c5: null, d5: null, e5: null, f5: null, g5: null, h5: null,
@@ -52,8 +52,9 @@ const gameSchema = new mongoose.Schema({
   },
 
   movedPiece: {
-    position: 
-    { type: String
+    position:
+    {
+      type: String
     }
   },
 
@@ -62,9 +63,9 @@ const gameSchema = new mongoose.Schema({
 
   whitePlayerId: { type: String, ref: 'User' },
   blackPlayerId: { type: String, ref: 'User' },
-  whitePlayerName: { type: String },  
+  whitePlayerName: { type: String },
   blackPlayerName: { type: String },
-  aiColor: { type: String, enum: ["white", "black", null], default: null },  
+  aiColor: { type: String, enum: ["white", "black", null], default: null },
 
   turnNumber: { type: Number, default: 0 },
 
